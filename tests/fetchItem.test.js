fa93740d5a75f9ed85c6c129427d572cfcaeeafb
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-13 19:34:02 -0300

    requisito-3.3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,17 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchItem é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Testa se fetch foi chamada', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/items/MLB1615760527')
+  });
+  it('Teste se o retorno da função com argumento MLB1615760527', async () => {
+    expect.assertions(1);
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
 });
