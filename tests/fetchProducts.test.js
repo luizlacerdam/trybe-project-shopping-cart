5aa832addd386ca9c0b936faae427633405601d6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-13 10:44:17 -0300

    requisito-1.4

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,18 +6,22 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
   it('Teste se é uma função', () => {
-    expect.assertions(1)
+    expect.assertions(1);
     expect(typeof fetchProducts).toBe('function');
   });
   it('Testa se fetch foi chamada', async () => {
-    expect.assertions(1)
-    await fetchProducts('computador')
+    expect.assertions(1);
+    await fetchProducts('computador');
     expect(fetch).toBeCalled();
   });
   it('Testa o endpoint ', async () => {
-    expect.assertions(1)
-    await fetchProducts('computador')
+    expect.assertions(1);
+    await fetchProducts('computador');
     const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
     expect(fetch).toBeCalledWith(url);
+  });
+  it('Testa o retorno da busca', async () => {
+    expect.assertions(1);
+    expect(fetchProducts('computador')).toEqual(computadorSearch);
   });
 });
