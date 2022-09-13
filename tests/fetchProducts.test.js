5ddacbee5580a5329756bf5508bbec75f0926371
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-13 10:42:39 -0300

    requisito-1.3

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,16 +2,22 @@ require('../mocks/fetchSimulator');
 const { expect } = require('@jest/globals');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
-console.log(computadorSearch);
+// console.log(computadorSearch);
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
   it('Teste se é uma função', () => {
     expect.assertions(1)
     expect(typeof fetchProducts).toBe('function');
   });
-  it('Testa se fetch foi chamada ', async () => {
+  it('Testa se fetch foi chamada', async () => {
     expect.assertions(1)
     await fetchProducts('computador')
     expect(fetch).toBeCalled();
+  });
+  it('Testa o endpoint ', async () => {
+    expect.assertions(1)
+    await fetchProducts('computador')
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toBeCalledWith(url);
   });
 });
