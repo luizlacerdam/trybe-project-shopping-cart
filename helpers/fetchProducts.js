5a9fbd5e1ef0403b5179241347940de7b9e77dc6
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-13 11:44:30 -0300

    requisito-2.0

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const url = (query) => `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  fetch(url('computador'))
+  .then((response) => response.json())
+  .then((data) => console.log(data.results))
+  .catch((error) => console.log(`Aconteceu esse error: ${error}`));
 };
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
