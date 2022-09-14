fbfde994a19595bf0586efb3fbd7ddaa9870abc1
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-09-14 16:59:08 -0300

    requisito-6

--- script.js
+++ script.js
@@ -119,5 +119,4 @@ criarItens();
 
 window.onload = () => { 
   addEventButoes();
-  
 };
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,18 @@
+const { expect } = require('@jest/globals');
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
 
-localStorageSimulator('setItem');
+localStorageSimulator('setItem'); //localstorage temporario (simulado)
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    saveCartItems('PC');
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
+    const valor = 'PC';
+    saveCartItems('PC');
+    expect(saveCartItems()).toBeCalledWith(localStorage.setItem('cartItems', valor));
+  });
 });
